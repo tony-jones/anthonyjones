f0c0c608c74e1878df41ea5398de20d8342a521c
Tony Jones <ad30jone@gmail.com>
2015-07-14 00:47:25 -0400

    twitter share was broken

--- share.php
+++ share.php
@@ -15,7 +15,9 @@
     if ( isset( $post ) ) {
       $url = urlencode( get_permalink( $post->ID ) );
       $title = urlencode( get_the_title( $post->ID ) );
-    } else {
+      $shortlink = urlencode( wp_get_shortlink( $post -> ID) );
+    } 
+    else {
       $url = get_bloginfo( 'url' );
       $title = get_bloginfo( 'name' );
     }
@@ -29,7 +31,7 @@
     <li><a href="https://plus.google.com/share?url='<?php echo $url; ?>" target="_blank" class="tooltipped btn-floating red" title="Share on Google +" data-position="left" data-delay="10" data-tooltip="Share on Google+"><i class="fa fa-google-plus"></i></a></li>
     <li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo $url; ?>&amp;title=<?php echo $title; ?>" target="_blank" class="tooltipped btn-floating blue darken-1" title="Share on Linkedin" data-position="left" data-delay="50" data-tooltip="Share on Linkedin"><i class="fa fa-linkedin"></i></a></li>
     <li><a href="https://www.facebook.com/sharer/sharer.php?u='<?php echo $url; ?>" target="_blank" class="tooltipped btn-floating indigo" title="Share on Facebook" data-position="left" data-delay="50" data-tooltip="Share on Facebook"><i class="fa fa-facebook"></i></a></li>
-    <li><a href="https://twitter.com/intent/tweet?original_referer=<?php echo wp_get_shortlink(); ?>&amp;text=<?php the_title(); ?>&tw_p=tweetbutton&url=<?php echo wp_get_shortlink(); ?>&via=iamtonybagels" target="_blank" class="tooltipped btn-floating blue lighten-1" title="Share on Twitter" data-position="left" data-delay="50" data-tooltip="Share on Twitter"><i class="fa fa-twitter"></i></a></li>
+    <li><a href="https://twitter.com/intent/tweet?original_referer=<?php echo $shortlink; ?>&amp;text=<?php echo $title; ?>&tw_p=tweetbutton&url=<?php echo $shortlink; ?>&via=iamtonybagels" target="_blank" class="tooltipped btn-floating blue lighten-1" title="Share on Twitter" data-position="left" data-delay="50" data-tooltip="Share on Twitter"><i class="fa fa-twitter"></i></a></li>
   </ul>
 </div>
 
@@ -45,7 +47,7 @@
     <a href="https://plus.google.com/share?url='<?php echo $url; ?>" target="_blank" class="social-share sharer btn red" title="Share on Google +"><i class="fa fa-google-plus"></i></a>
     <a href="https://www.facebook.com/sharer/sharer.php?u='<?php echo $url; ?>" target="_blank" class="social-share sharer btn blue darken-2" title="Share on Facebook"><i class="fa fa-facebook"></i></a>
     <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo $url; ?>&amp;title=<?php echo $title; ?>" target="_blank" class="social-share sharer btn blue darken-3" title="Share on Linkedin"><i class="fa fa-linkedin"></i></a>
-    <a href="https://twitter.com/intent/tweet?original_referer=<?php echo wp_get_shortlink(); ?>&amp;text=<?php the_title(); ?>&tw_p=tweetbutton&url=<?php echo wp_get_shortlink(); ?>&via=iamtonybagels" class="social-share sharer btn blue" title="Share on Twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+    <a href="https://twitter.com/intent/tweet?original_referer=<?php echo $shortlink; ?>&amp;text=<?php echo $title; ?>&tw_p=tweetbutton&url=<?php echo $shortlink; ?>&via=iamtonybagels" target="_blank" class="tooltipped btn-floating blue lighten-1" title="Share on Twitter" data-position="left" data-delay="50" data-tooltip="Share on Twitter"><i class="fa fa-twitter"></i></a>
   </div>
   <div class="modal-footer">
     <a href="#!" class="modal-action modal-close waves-effect waves-orange btn-flat">Close</a>
