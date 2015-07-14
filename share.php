3146e0004b8c9400570f5452e41f3f00195562da
Tony Jones <ad30jone@gmail.com>
2015-07-14 00:49:06 -0400

    fixed twitter styling

--- share.php
+++ share.php
@@ -47,7 +47,7 @@
     <a href="https://plus.google.com/share?url='<?php echo $url; ?>" target="_blank" class="social-share sharer btn red" title="Share on Google +"><i class="fa fa-google-plus"></i></a>
     <a href="https://www.facebook.com/sharer/sharer.php?u='<?php echo $url; ?>" target="_blank" class="social-share sharer btn blue darken-2" title="Share on Facebook"><i class="fa fa-facebook"></i></a>
     <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo $url; ?>&amp;title=<?php echo $title; ?>" target="_blank" class="social-share sharer btn blue darken-3" title="Share on Linkedin"><i class="fa fa-linkedin"></i></a>
-    <a href="https://twitter.com/intent/tweet?original_referer=<?php echo $shortlink; ?>&amp;text=<?php echo $title; ?>&tw_p=tweetbutton&url=<?php echo $shortlink; ?>&via=iamtonybagels" target="_blank" class="tooltipped btn-floating blue lighten-1" title="Share on Twitter" data-position="left" data-delay="50" data-tooltip="Share on Twitter"><i class="fa fa-twitter"></i></a>
+    <a href="https://twitter.com/intent/tweet?original_referer=<?php echo $shortlink; ?>&amp;text=<?php echo $title; ?>&tw_p=tweetbutton&url=<?php echo $shortlink; ?>&via=iamtonybagels" target="_blank" class="social-share sharer btn blue lighten-1" title="Share on Twitter" data-position="left" data-delay="50" data-tooltip="Share on Twitter"><i class="fa fa-twitter"></i></a>
   </div>
   <div class="modal-footer">
     <a href="#!" class="modal-action modal-close waves-effect waves-orange btn-flat">Close</a>
